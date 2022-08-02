650f48ef0915454d4a810424e09e8aaf2f6dddf5
Naman Parashar <namanparashar484@gmail.com>
2022-08-02 22:44:31 +0530

    Cp

---
+++ src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        String str1 = "java";
+        String str2 = "java";
+        System.out.println(str1.equals(str2));
+        System.out.println(str1==str2);
+    }
+}
