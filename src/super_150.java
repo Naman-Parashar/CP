346c567dd5442e70e35916be3967c717984668f5
Naman Parashar <namanparashar484@gmail.com>
2022-08-07 00:25:57 +0530

    rename

--- src/super_150.java
+++ src/super_150.java
@@ -1,6 +1,4 @@
-import java.util.Scanner;
-
-public class test {
+public class super_150 {
 //        public static void main(String args[]) {
 //            Scanner sc = new Scanner(System.in);
 //            String str = "abc";
