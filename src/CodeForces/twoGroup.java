29010fbd4d8e9edcad0246132fcaad7fca1e38b1
Naman Parashar <namanparashar484@gmail.com>
2022-11-15 09:02:35 +0530

    Codeforces

---
+++ src/CodeForces/twoGroup.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class twoGroup {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long t=sc.nextLong();
+        while(t-->0){
+            int n=sc.nextInt();
+            long sum=0;
+            for(int i=0;i<n;i++){
+                sum+=sc.nextLong();
+            }
+            System.out.println(Math.abs(sum));
+        }
+    }
+}
