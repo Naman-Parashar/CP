4099333b48dd037190a94fc42af031c5655ce09b
Naman Parashar <namanparashar484@gmail.com>
2022-11-19 18:42:14 +0530

    p

---
+++ src/CodeForces/_4_Kevin_and_Permutation.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class _4_Kevin_and_Permutation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t  = sc.nextInt();
+
+        while (t-- > 0){
+            int l = sc.nextInt();
+            int d = l/2;
+
+            for (int j = 0; j < d; j++) {
+                System.out.print((d+j+1)+" "+(j+1)+" ");
+            }
+            System.out.println((l%2 != 0) ? l :"");
+        }
+    }
+}
